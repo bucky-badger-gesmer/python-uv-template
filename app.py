65d24ffcc747100131c7b8a778c0297efa4c2f95
Aaron Gesmer <agesmer@augeomarketing.com>
2025-07-15 09:46:21 -0500

    feat: enhance app.py example

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
+import json
 from typing import Any
 
 from requests import RequestException, Response, get
 from rich import pretty
 
-url: str = "https://stats.nba.com/stats/playerdashboardbyyearoveryearcombined?DateFrom=&DateTo=&GameSegment=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerID=1630162&PlusMinus=N&Rank=N&Season=2024-25&SeasonSegment=&SeasonType=Regular Season&ShotClockRange=&VsConference=&VsDivision="
+player_id: int = 1630162  # Anthony Edwards
+url: str = (
+    f"https://stats.nba.com/stats/playerdashboardbyyearoveryearcombined?"
+    f"DateFrom=&DateTo=&GameSegment=&LastNGames=0&LeagueID=00&Location=&"
+    f"MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&"
+    f"PerMode=PerGame&Period=0&PlayerID={player_id}&PlusMinus=N&Rank=N&"
+    f"Season=2024-25&SeasonSegment=&SeasonType=Regular Season&ShotClockRange=&"
+    f"VsConference=&VsDivision="
+)
+
 
 headers: dict[str, str] = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:140.0) Gecko/20100101 Firefox/140.0^",
@@ -21,8 +31,20 @@ def get_player_dashboard_data(
     return response.json()
 
 
-try:
-    data: dict[str, Any] = get_player_dashboard_data(url, headers)
-    pretty.pprint(data)
-except RequestException as e:
-    print(f"An error occurred while fetching the data: {e}")
+def dump_json_to_file(data: dict[str, Any], filename: str) -> None:
+    with open(filename, "w") as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+
+    pretty.pprint(
+        f'Dashboard data for player ID {player_id} saved to "{filename}" successfully ✅'
+    )
+
+
+if __name__ == "__main__":
+    try:
+        data: dict[str, Any] = get_player_dashboard_data(url, headers)
+        filename: str = f"{player_id}_dashboard_data.json"
+
+        dump_json_to_file(data, filename)
+    except RequestException as e:
+        print(f"An error occurred while fetching the data: {e}")
